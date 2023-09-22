3d4ebc3a7ac29f81a7f8e714545041c0a78fe302
Cosmin <cosmin_7d@yahoo.com>
2023-09-22 15:54:21 +0100

    changes made

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 // Function to toggle section visibility based on scroll position
 function toggleSectionVisibility() {
-    const section = document.getElementById('about');
+    const section = document.querySelector('#about');
+
     const rect = section.getBoundingClientRect();
+    console.log(window.innerHeight);
+    console.log(rect);
 
     // Show when section is in viewport
     if (rect.top < window.innerHeight && rect.bottom >= 0) {
-        section.classList.add('active');
+        rect.classList.add('active');
     } else {
-        section.classList.remove('active');
+        rect.classList.remove('active');
     }
 }
 
-window.addEventListener('scroll', toggleSectionVisibility);
+// window.addEventListener('scroll', toggleSectionVisibility);
+
+let modal = document.querySelector("#my-modal");
+
+let img = document.querySelector("#my-img");
+
+let modalImg = document.querySelector("#img01");
+
+let captionText = document.querySelector("#caption");
+
+img.onclick = function () {
+    console.log("inside");
+    modal.style.display = "block";
+    modalImg.style.width = "60%";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+let span = document.querySelectorAll(".close")[0];
+console.log(span);
+span.onclick = () => modal.style.display = "none";
