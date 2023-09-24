ad91279067e8ca7b80181cc699ff909827c5ce01
Cosmin <cosmin_7d@yahoo.com>
2023-09-24 16:42:23 +0100

    changes made

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Function to toggle section visibility based on scroll position
-const text ="Hi, I'm Cosmin, a passionate full-stack developer eager to innovate and create. My journey into coding started with a desire to switch careers, leading me to join the  award-winning Full Stack Track Course at iO Academy. In there I have enforced my skills on languages like HTML, CSS and JAVASCRIPT but also had the opportunity to learn the PHP language, also I have had the chance to work with frameworks like REACT and Slim. I enjoyed working in collaborative team environments, following Agile methodologies and delivering successful projects. To get a better glimpse to what I've done, please explore my projects below :)";
+const text ="Hello there! I'm Cosmin, a passionate full-stack developer who is always looking to innovate and create meaningful solutions. I had the opportunity to develop my skills in HTML, CSS, JS, PHP and MySQL at iO Academy, where I joined their award-winning bootcamp Full Stack Track Course. Also, in there I explored frameworks like REACT and Slim. Followed Agile methodologies and worked in collaborative team environments to deliver successful projects. For a better glimpse of my work, please explore my projects below."
 let charIndex= 0;
 function type() {
     const element = document.querySelector("#typewriter-text");
     element.textContent += text.charAt(charIndex);
     charIndex++;
     if(charIndex < text.length) {
-        setTimeout(type,25);
+        setTimeout(type,30);
     }
 }
 type();
